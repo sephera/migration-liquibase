b7346baf54d84028f66966153b43bafdf1948910
Hai Nguyen <haind.vng@gmail.com>
2020-04-08 10:48:33 +0700

    init project

---
+++ src/main/java/com/example/migrationliquibase/MigrationLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.migrationliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationLiquibaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/migrationliquibase/MigrationLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migrationliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
